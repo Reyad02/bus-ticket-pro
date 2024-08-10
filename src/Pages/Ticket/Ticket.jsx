36c5c47d8ec66ee606e25b0d001eff913b3772f5
unknown <areyad203011@bscse.uiu.ac.bd>
2024-08-10 16:38:29 +0600

    filter part done

--- src/Pages/Ticket/Ticket.jsx
+++ src/Pages/Ticket/Ticket.jsx
@@ -1,27 +1,81 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 // import useBuses from "../../Hooks/useBuses";
 import { getPoints } from "../../utils/storage";
 import { BusContext } from "../../provider/BusProvider";
 import { Helmet } from "react-helmet-async";
 import Banner from "../Shared/Banner/Banner";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axiosBusInfo from "../../utils/axiosBusInfo";
 import { FaBus } from "react-icons/fa";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 
+
 const Ticket = () => {
     const { pickPoint, dropPoint } = getPoints();
     const { bus, setBus } = useContext(BusContext);
-    console.log(Array.isArray(bus));
-    // console.log(pickPoint);
-    // console.log(dropPoint);
+    const [filterBus, setFilterBus] = useState([]);
+    const [acBus, setAcBus] = useState(false);
+    const [nonAcBus, setNonAcBus] = useState(false);
+
+    const handleAcBus = (value, checked) => {
+        if (checked && value && !nonAcBus) {
+            setAcBus(true)
+            const filteredBus = bus.filter((eachBus) => eachBus.type === value);
+            setFilterBus(filteredBus);
+        }
+        else if (checked && value && nonAcBus) {
+            setAcBus(true)
+            setFilterBus(bus);
+        }
+        else if (!checked && nonAcBus) {
+            setAcBus(false);
+            const filteredBus = bus.filter((eachBus) => eachBus.type === "Non-AC");
+            setFilterBus(filteredBus);
+        }
+        else if (!checked && !nonAcBus) {
+            setAcBus(false);
+            setNonAcBus(false)
+            setFilterBus(bus);
+        }
+        else {
+            setFilterBus(bus);
+        }
+    }
+
+    const handleNonAcBus = (value, checked) => {
+        if (checked && value && !acBus) {
+            setNonAcBus(true)
+            const filteredBus = bus.filter((eachBus) => eachBus.type === value);
+            setFilterBus(filteredBus);
+        }
+        else if (checked && value && acBus) {
+            setNonAcBus(true)
+            setFilterBus(bus);
+        }
+        else if (!checked && acBus) {
+            setNonAcBus(false);
+            const filteredBus = bus.filter((eachBus) => eachBus.type === "AC");
+            setFilterBus(filteredBus);
+        }
+        else if (!checked && !acBus) {
+            setAcBus(false);
+            setNonAcBus(false)
+            setFilterBus(bus);
+        }
+        else {
+            setFilterBus(bus);
+        }
+
+    }
+
+
     useEffect(() => {
         axiosBusInfo({ pickupPoint: pickPoint, droppingPoint: dropPoint })
             .then(response => {
-                // console.log(response.data);
                 setBus(response.data)
+                setFilterBus(response.data);
             })
             .catch(error => {
                 console.error('Error making GET request:', error);
@@ -44,24 +98,32 @@ const Ticket = () => {
                         </label>
                     </div>
                     <div className="drawer-side lg:ml-12 mt-12">
-                        <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 rounded">
+                        {/* <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> */}
+                        <div className="menu bg-base-200 text-base-content min-h-full w-80 p-4 rounded">
+                            <p className="text-[#424248] font-semibold text-2xl mt-2 pb-6 border-b-2 ">Filter</p>
+                            <p className="text-[#424248] font-semibold text-xl mt-6">Vehicle Type</p>
                             {/* Sidebar content here */}
-                            <li><a>Sidebar Item 1</a></li>
-                            <li><a>Sidebar Item 2</a></li>
-                        </ul>
+                            <div className="mt-4">
+                                <div className="flex items-center mb-4">
+                                    <input id="default-checkbox" name="list" type="checkbox" value="AC" className="w-4 h-4   border-[#424248] rounded   " onClick={(e) => handleAcBus(e.target.value, e.target.checked)} />
+                                    <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-[424248]">AC</label>
+                                </div>
+                                <div className="flex items-center">
+                                    <input id="default-checkbox" name="list" type="checkbox" value="Non-AC" className="w-4 h-4   border-[#424248] rounded   " onClick={(e) => handleNonAcBus(e.target.value, e.target.checked)} />
+                                    <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-[424248]">Non-AC</label>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
                 <div className="lg:w-3/4 mt-12">
-                    {/* {
-                        bus.map((eachBus) => <div key={eachBus.bus_num}>{eachBus}</div>)
-                    } */}
                     <div className="mx-auto max-w-5xl">
                         {
-                            bus.length > 0 ? (
-                                bus.map((eachBus) => (
+                            filterBus.length > 0 ? (
+                                filterBus.map((eachBus) => (
                                     <div key={eachBus.bus_num} className="mb-8 bg-base-200 p-4 rounded  px-10 text-[#424248]">
-                                        <div className="flex justify-between border-b-2 pb-2">
+                                        <div className="flex justify-between border-b-2 pb-4">
                                             <div className="space-y-1">
                                                 <p className="font-semibold text-xl">Bus Num - <span className="uppercase">{eachBus.bus_num}</span></p>
                                                 <p className="text-xs">Seat Layout - {eachBus.seat_layout}</p>
@@ -80,9 +142,9 @@ const Ticket = () => {
                                                 <Link className="text-white bg-[#15803D] rounded-md py-1 px-2">Select Seat</Link>
                                             </div>
                                         </div>
-                                        <div className="pt-2 flex gap-2 text-lg">
+                                        <div className="pt-4 flex gap-2 text-lg">
                                             <p className="text-[#777777] flex font-semibold">Facilities - </p>
-                                            <div className="flex gap-2">
+                                            <div className="flex gap-4">
 
                                                 {
                                                     eachBus.facilities.map((facility) => <div key={facility.name} className="">
