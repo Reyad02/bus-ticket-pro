fe5f79740616dfde08d8f283dbd43a04fbdfa05b
unknown <areyad203011@bscse.uiu.ac.bd>
2024-08-23 21:13:17 +0600

    localstorage clear when payment failed

--- src/Pages/PaymentFail/PaymentFail.jsx
+++ src/Pages/PaymentFail/PaymentFail.jsx
@@ -2,9 +2,12 @@ import { useParams } from "react-router-dom";
 
 const PaymentFail = () => {
     const { id } = useParams();
+    localStorage.removeItem("seats");
+    localStorage.removeItem("bus_name");
     return (
         <div className="max-w-7xl mx-auto">
             payment failed: {id}
+
         </div>
     );
 };
